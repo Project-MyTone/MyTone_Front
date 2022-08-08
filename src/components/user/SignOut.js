e18694030282e81e0622299d854069baa874368a
mincheolsong <thdalscjf05@naver.com>
2022-08-08 18:16:04 +0900

    로그아웃 컴포넌트

---
+++ src/components/user/SignOut.js
@@ -0,0 +1,39 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+
+import { logoutUser } from '../../api/url';
+import { getCookieToken,removeCookieToken } from '../../cookie/Cookie';
+import { deleteToken } from '../../store';
+
+function Logout(){
+    const {accessToken} = useSelector(state=>state.token);
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    
+    const refreshToken = getCookieToken();
+
+    async function logout(){
+        const response = await logoutUser(refreshToken);
+        
+        if(response.status){
+            dispatch(deleteToken());
+            removeCookieToken();
+            return navigate('/signup');
+        }else{
+            alert('로그아웃 실패ㅜ.ㅜ');
+        }
+    }
+    useEffect(()=>{
+        logout()
+    },[]);
+
+    return(
+        <>
+            <Link to="/signup" />
+        </>
+    )
+}
