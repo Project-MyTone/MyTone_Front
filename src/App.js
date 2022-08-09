d47b9acaedcadb26ce21d573db153b3ee279a2b0
mincheolsong <thdalscjf05@naver.com>
2022-08-09 22:58:57 +0900

    category bar 에러 수정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { react, useState, Suspense } from 'react'
+import { react, useState, Suspense, useEffect } from 'react'
 import { Route, Routes, Link, useLocation } from 'react-router-dom'
 import ArticlePost from './components/board/ArticlePost.js'
 import ArticleEdit from './components/board/ArticleEdit.js'
-import BoardMain from './components/board/BoardMain'
 import ArticleDetail from './components/board/ArticleDetail'
+import ArticleCategory from './components/board/ArticleCategory.js'
+import Article from './components/board/Article.js'
 import Main from './Main/Main'
 import Header from './Layout/Header'
 import Footer from './Layout/Footer'
@@ -13,21 +14,26 @@ import ImgUpload from './Test/ImgUpload'
 import Result from './Test/Result'
 import Loading from './components/loading/Loading'
 import PrivateRoute from './routes/PrivateRoute.js'
+import axios from "axios"
+
 function App() {
   let [category, setCategory] = useState(-1);
-
-
+  
+ 
   return (
     <div className="App">
       <Header></Header>
-
+      
       <Suspense fallback={<Loading />}>
         <Routes>
           <Route path="/" element={<Main />}></Route>
           <Route path="/signup" element={<SignUp></SignUp>}></Route>
           <Route path="/signin" element={<SignIn></SignIn>}></Route>
-          <Route path="/board" element={<BoardMain category={category} setCategory={setCategory} ></BoardMain>}></Route>
-          <Route path="/detail/:id" element={<ArticleDetail setCategory={setCategory}></ArticleDetail>}></Route>
+          <Route path="/board" element={<ArticleCategory category={category} setCategory={setCategory} ></ArticleCategory>}>
+            <Route path="list" element={<Article category={category}></Article>}></Route>
+            <Route path="detail/:id" element={<ArticleDetail setCategory={setCategory}></ArticleDetail>}></Route>
+          </Route>
+          
           {/* <Route element={<PrivateRoute></PrivateRoute>}> */}
             <Route path="/post" element={<ArticlePost></ArticlePost>}></Route>
             <Route path="/edit/:id" element={<ArticleEdit></ArticleEdit>}></Route>
