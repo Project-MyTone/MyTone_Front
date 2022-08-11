49ab82e50ee4bc3293d32b10f8d8873159a7fd4c
mincheolsong <thdalscjf05@naver.com>
2022-08-11 23:24:35 +0900

    do nothing

--- src/App.js
+++ src/App.js
@@ -17,13 +17,14 @@ import PrivateRoute from './routes/PrivateRoute.js'
 import axios from "axios"
 
 function App() {
-  let [category, setCategory] = useState(-1); // -1:all, 0:spring-warm, 1:summer-cool, 2:fall-warm, 3:winter-cool 
-                                
+  let [category, setCategory] = useState(-1); // 0:all, 1:여름 쿨톤, 2:겨울 쿨톤, 3:가을 웜톤, 4:봄 웜톤
+ 
+
+  
  
   return (
     <div className="App">
       <Header></Header>
-      
       <Suspense fallback={<Loading />}>
         <Routes>
           <Route path="/" element={<Main />}></Route>
@@ -35,7 +36,7 @@ function App() {
             <Route path="detail/:id" element={<ArticleDetail setCategory={setCategory}/>}></Route> {/*/board/detail/:id로 접속하면 카테고리와 상세 게시판을 보여줌*/}
           </Route>
           
-          {/* <Route element={<PrivateRoute></PrivateRoute>}> */} {/*로그인이 된 경우만 접근할 수 있는 Route*/}
+          {/* <Route element={<PrivateRoute></PrivateRoute>}> 로그인이 된 경우만 접근할 수 있는 Route */}
             <Route path="/post" element={<ArticlePost></ArticlePost>}></Route>
             <Route path="/edit/:id" element={<ArticleEdit></ArticleEdit>}></Route>
             <Route path="/image" element={<ImgUpload></ImgUpload>}></Route>
