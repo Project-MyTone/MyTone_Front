af5b89332be1cf2ec84c88d1a8ed6140aa9a2d96
mincheolsong <thdalscjf05@naver.com>
2022-08-11 23:28:44 +0900

    modify cookie

--- src/cookie/Cookie.js
+++ src/cookie/Cookie.js
@@ -5,10 +5,10 @@ import { Cookies } from "react-cookie";
 const cookies = new Cookies();
 
 export const setRefreshToken = (refreshToken)=>{
-    const today = new Date();
-    const expireDate = today.setDate(today.getDate() + 7);
+    //const today = new Date();
+    //const expireDate = today.setDate(today.getDate() + 7);
 
-    return cookies.set('refresh_token'.refreshToken,{sameSite:'strict',path:"/",expires:new Date(expireDate)});
+    return cookies.set('refresh_token',refreshToken,{sameSite:'strict',path:"/"});
 };
 
 export const getRefreshToken = ()=>{
