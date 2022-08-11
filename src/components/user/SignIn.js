7775007aa15596ce3487c68cd05cf8edbf7b20a7
mincheolsong <thdalscjf05@naver.com>
2022-08-11 23:27:57 +0900

    add axios login

--- src/components/user/SignIn.js
+++ src/components/user/SignIn.js
@@ -2,12 +2,12 @@ import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { useForm } from 'react-hook-form';
-
+import axios from 'axios';
 import { HiLockClosed } from 'react-icons/hi'
 import { ErrorMessage } from '@hookform/error-message';
-
+import { useSelector } from 'react-redux';
 import { loginUser } from '../../api/url';
-import { setRefreshToken } from '../../cookie/Cookie';
+import { getRefreshToken, setRefreshToken } from '../../cookie/Cookie';
 import { setAuthToken } from '../../store';
 
 import { Button } from 'react-bootstrap'
@@ -19,7 +19,7 @@ function SignIn() {
     // let [pw2, setPw2] = useState('')
     const navigate = useNavigate();
     const dispatch = useDispatch();
-
+    let state = useSelector((state)=>state)
     // useForm 사용을 위한 선언
     const { register, setValue, formState: { errors }, handleSubmit } = useForm();
 
@@ -27,25 +27,43 @@ function SignIn() {
     // 백으로 유저 정보 전달
     const onValid = async ({ username, password }) => {
         const response = await loginUser({ username, password });
+        console.log(response)
+        // if (response.status) {
+        //     // 쿠키에 Refresh Token, store에 Access Token 저장
+        //     setRefreshToken(response.json.token.refresh_token);
+        //     dispatch(setAuthToken(response.json.token.access_token));
 
-        if (response.status) {
-            // 쿠키에 Refresh Token, store에 Access Token 저장
-            setRefreshToken(response.json.token.refresh_token);
-            dispatch(setAuthToken(response.json.token.access_token));
-
-            return navigate("/");
-        } else {
-            console.log(response.json);
-        }
-        // input 태그 값 비워주는 코드
-        setValue("password", "");
+        //     return navigate("/");
+        // } else {
+        //     console.log(response.json);
+        // }
+        // // input 태그 값 비워주는 코드
+        // setValue("password", "");
     };
 
     return (
         <div className='login'>
             <h3>회원 로그인</h3>
             <div className="login-box">
-                <form onSubmit={handleSubmit(onValid)}>
+                <form onSubmit={ (e)=>{
+                        e.preventDefault();
+                        axios.post('/user/login/', {
+                            username: e.target.username.value,
+                            password: e.target.password.value,
+                        })
+                        .then((res)=>{
+                            if(res.status==200){
+                                alert(res.data.message)
+                                setRefreshToken(res.data.token.refresh_token);
+                                dispatch(setAuthToken(res.data.token.access_token));
+                                navigate("/");
+                            }
+                        })
+                        .catch((err)=>{
+                            console.log(err)
+                            setValue("password", "");
+                        })
+                    }}>
                     <div className='id-container'>
                         <div className="id">아이디</div>
                         <input
