5eadc276c30066876ae77fe99c614e32df409b8f
mincheolsong <thdalscjf05@naver.com>
2022-08-15 00:09:52 +0900

    게시판 작성 이미지 업로드 추가

--- src/components/board/ArticlePost.js
+++ src/components/board/ArticlePost.js
@@ -4,14 +4,24 @@ import { useNavigate } from 'react-router';
 import { addArticle } from '../../store.js'
 import {Dropdown,Button} from 'react-bootstrap'
 import './ArticlePost.css'
+import axios from 'axios';
 function ArticlePost() {
     let [title, setTitle] = useState('');
     let [content, setContent] = useState('');
-    let [board, setBoard] = useState({ title: '', content: '', img: '', board: '', user: '', create_at: '' });
-    let [category,setCategory] = useState('0')
+    let [image,setImage] = useState(''); // 이미지
+    let [category,setCategory] = useState(1);
+    
     let dispatch = useDispatch();
     let navigate = useNavigate();
     
+    let accessToken=localStorage.getItem('accessToken');
+
+    let onLoadFile = (e)=>{ // 이미지
+        const file = e.target.files;
+        //console.log(file);
+        setImage(file);
+    }
+
     
     return (
         <div className='post-top'>
@@ -21,38 +31,71 @@ function ArticlePost() {
                 </div>
 
                 <Dropdown>
-                    <Dropdown.Toggle variant="secondary" id='dropdown-variants-Secondary'>
-                        {category=='0'?'Cool':'Warm'}
+                    <Dropdown.Toggle variant="success" id='dropdown-basic'>
+                        {
+                        category=='1'
+                        ?'여름 쿨톤'
+                        :category=='2'
+                        ?'겨울 쿨톤'
+                        :category=='3'
+                        ?'가을 웜톤'
+                        :'봄 웜톤'
+                        }
                     </Dropdown.Toggle>
 
                     <Dropdown.Menu>
-                        <Dropdown.Item onClick={()=>{setCategory('0')}}>Cool</Dropdown.Item>
-                        <Dropdown.Item onClick={()=>{setCategory('1')}}>Warm</Dropdown.Item>
+                        
+                        <Dropdown.Item onClick={()=>{setCategory(1)}}>여름 쿨톤</Dropdown.Item>
+                        <Dropdown.Item onClick={()=>{setCategory(2)}}>겨울 쿨톤</Dropdown.Item>
+                        <Dropdown.Item onClick={()=>{setCategory(3)}}>가을 웜톤</Dropdown.Item>
+                        <Dropdown.Item onClick={()=>{setCategory(4)}}>봄 웜톤</Dropdown.Item>
                     </Dropdown.Menu>
                 </Dropdown>
             </div>
             <br></br>
-            <div style={{width:'100%', height:'100%',marginBottom:'15px'}}>
+            <div style={{width:'100%', height:'75%',marginBottom:'15px'}}>
                 <textarea style={{width:'100%', height:'100%'}} name="content" onChange={(e) => { setContent(e.target.value) }}></textarea>
             </div>
-            
+            <form>
+                <input type='file' accept="img/*" onChange={onLoadFile}></input>
+            </form>
             <Button variant="light" onClick={() => {
-                const date = new Date();
-                const year = date.getFullYear();
-                const month = ('0' + (date.getMonth() + 1)).slice(-2);
-                const day = ('0' + date.getDay()).slice(-2);
-                const dateStr = year + '-' + month + '-' + day;
+                const formdata = new FormData();
+                console.log(image[0]); //이미지
+                formdata.append('image',image[0]); //이미지
+                formdata.append('title',title);
+                formdata.append('content',content);
+                formdata.append('board',parseInt(category));
 
-                let tmp = board;
-                tmp.title = title;
-                tmp.content = content;
-                tmp.board = category;
-                tmp.create_at = dateStr;
-                setBoard(tmp);
-                dispatch(addArticle(board))
-                alert('작성완료')
-                navigate('/board')
-            }}>작성완료</Button>
+                // var object={}
+                // formdata.forEach((value,key)=>{
+                //     object[key]=value
+                // })
+                // console.log(object.images)
+
+                // const id = 1;
+                // const image = object.images.
+                axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`
+                
+                axios.post('/article/',formdata,{
+                    headers : {
+                        "Content-Type" : "multipart/form-data"
+                    }
+                })            
+                .then((res)=>{
+                    if(res.status==201){
+                        alert(res.statusText)
+                        navigate('/board/list')
+                    }
+                })
+                .catch((err)=>{
+                    
+                    alert('게시글을 작성하려면 로그인이 필요합니다')
+                    navigate('/signin')
+                    
+                })
+                
+                }}>작성완료</Button>
         </div>
     )
 }
