ca94b3730512ea673046475a0ada1a0e934fa30f
mincheolsong <thdalscjf05@naver.com>
2022-08-17 21:20:32 +0900

    Update Comment and Add Recomment

--- src/components/board/ArticleDetail.js
+++ src/components/board/ArticleDetail.js
@@ -1,216 +1,366 @@
-import {useSelector} from 'react-redux'
-import {useNavigate, useParams} from 'react-router-dom'
-import {useDispatch} from 'react-redux'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
 import { Button, InputGroup, Form } from 'react-bootstrap'
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 import { removeRefreshToken } from '../../cookie/Cookie'
 import { deleteAuthToken } from '../../store'
 import './ArticleDetail.css'
 
 import axios from 'axios'
 
-function Comment(props){
-    let [comment,setComment] = useState('');
-    let [commentList,setCommentList] = useState([]);
-    const dispatch = useDispatch();
 
-    useEffect(()=>{
-        axios.get(`/comment/?limit=20&offset=0`)
-        .then((res)=>{
-            if(res.status==200){
-                setCommentList(res.data.results.filter((e) => e.article == props.id))
-                
-                console.log(res.data.results.filter((e) => e.article == props.id))
-                //console.log(res.data.results)
-            }
-        })
-        .catch((err)=>{console.log(err)})
+function Recomment(props) {
+	let [recomment, setRecomment] = useState('');
+	const dispatch = useDispatch();
+	return (
+		<InputGroup className="mb-3" style={{ width: '60%' }}>
+			<p>↳</p>
+			<Form.Control
+				placeholder="대댓글남기기"
+				aria-label="Recipient's username"
+				aria-describedby="basic-addon2"
+				onClick={(e) => { e.target.value = '' }}
+				onChange={(e) => { setRecomment(e.target.value) }}
+			/>
+			<Button variant="outline-secondary" id="button-addon2" onClick={() => {
+				axios.defaults.headers.common['Authorization'] = `Bearer ${props.accessToken}`
+				axios.post('/comment/recomment/', {
+					body: recomment,
+					comment: props.id
+				})
+					.then((res) => {
+						if (res.status == 201) {
+							console.log(res)
+							window.location.reload()
+						}
 
-    },[])
+					})
+					.catch((err) => {
+						console.log(err)
+						if (err.response.status == 401) {
+							alert('로그인을 해주세요')
+							removeRefreshToken();
+							dispatch(deleteAuthToken());
+							window.location.reload()
+						}
 
-    return(
-        <div className='comment-top'>
-            <h4>댓글</h4>
-            
-                {
-                    
-                    commentList.map((a,i)=>{
-                        return(
-                            
-                            <div key={i} className='comment'>
-                                <div className='comment-content'>
-                                    <div style={{fontSize:'large'}}>{a.body}</div>
-                                </div>
-                                <div>작성자 : {a.user}</div>
-                            </div>
-                            
-                        )
-                    })
-                }
-            
-            <InputGroup className="mb-3" style={{ height: '44px', width: '100%', minWidth: '350px' }}>
-                    <Form.Control
-                        placeholder="댓글을 작성해주세요"
-                        aria-label="Recipient's username"
-                        aria-describedby="basic-addon2"
-                        onClick={(e)=>{e.target.value=''}}
-                        onChange={(e) => {setComment(e.target.value)}}
-                    />
-                    
-                    <Button variant="outline-secondary" id="button-addon2" onClick={() => {
-                        axios.defaults.headers.common['Authorization'] = `Bearer ${props.accessToken}`
-                        axios.post('/comment/',{
-                            body:comment,
-                            article:props.id
-                        })
-                        .then((res)=>{
-                            if(res.status==201){
-                                console.log(res)
-                                window.location.reload()
-                            }
-                            
-                        })
-                        .catch((err)=>{
-                            console.log(err)
-                            if(err.status==401){
-                                alert('로그인을 해주세요')
-                                removeRefreshToken();
-                                dispatch(deleteAuthToken());
-                                window.location.reload()
-                            }
-                        
-                        })
-                    }}>
-                        등록
-                    </Button>
+					})
+			}}>
+				등록
+			</Button>
+		</InputGroup>
+	)
+}
+function Comment(props) {
+	let [comment, setComment] = useState('');
+	let [commentList, setCommentList] = useState([]);
 
-            </InputGroup>
-        </div>
-    )
+	let [editClicked, setEditClicked] = useState(false);
+	let [editCommentId, setEditCommentId] = useState();
+	let [edittingComment, setEdittingComment] = useState('')
+
+
+	let [recommentClicked, setRecommentClicked] = useState(false);
+	let [recommentId, setRecommentId] = useState();
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		axios.get(`/comment/`)
+			.then((res) => {
+				if (res.status == 200) {
+					setCommentList(res.data.results.filter((e) => e.article == props.id))
+
+					// console.log(res.data.results.filter((e) => e.article == props.id))
+					//console.log(res.data.results)
+				}
+			})
+			.catch((err) => { console.log(err) })
+	}, [])
+
+	function editComment() {
+		if (props.accessToken != null) {
+			axios.defaults.headers.common['Authorization'] = `Bearer ${props.accessToken}`
+
+			axios.patch(`/comment/${editCommentId}/`, {
+				body: edittingComment,
+
+			})
+				.then((res) => {
+					console.log(res)
+					if (res.status == 200) {
+						alert('수정완료')
+						setEditClicked(false)
+						window.location.reload()
+					}
+				})
+				.catch((err) => {
+					//alert(err.response.data.detail)
+
+					//console.log(err.response.status)
+					if (err.response.status == 403) {
+						alert('본인의 댓글만 수정 가능합니다')
+						window.location.reload()
+					}
+
+				})
+		} else {
+			alert('로그인 후 수정할 수 있습니다')
+			window.location.reload()
+		}
+	}
+
+	return (
+		<div className='comment-top'>
+			<h4>댓글</h4>
+
+			{
+
+				commentList.map((a, i) => {
+					return (
+						<div key={i}>
+							<div className='comment'>
+								{
+									editClicked == true && editCommentId == a.id
+										?
+										<div className='comment-content'>
+
+											<textarea style={{ width: '100%', wordBreak: 'break-all' }} value={edittingComment} onChange={(e) => { setEdittingComment(e.target.value) }}></textarea>
+										</div>
+										:
+										<div className='comment-content'>
+											{/* {console.log(a)} */}
+											<div style={{ fontSize: 'large', width: '100%', wordBreak: 'break-all' }}>{a.body}</div>
+										</div>
+
+								}
+
+								<div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', width: '140px' }}>
+									<div style={{ display: 'flex', justifyContent: 'space-between' }}>
+										<p>작성자 : {a.user}</p>
+										<Button style={{ padding: '0', height: '26px', width: '26px' }} variant="danger" onClick={() => {
+											axios.defaults.headers.common['Authorization'] = `Bearer ${props.accessToken}`
+											axios.delete(`/comment/${a.id}/`)
+												.then((res) => {
+													if (res.status == 204) {
+														alert('댓글이 삭제되었습니다.')
+														window.location.reload();
+													}
+
+												})
+												.catch((err) => {
+													if (err.response.status == 403) {
+														alert('본인의 댓글만 삭제 할 수 있습니다!')
+													}
+													else if (err.response.status == 401) {
+														alert('권한이 없습니다!')
+														window.location.reload();
+													}
+												})
+										}}>X</Button>
+
+									</div>
+									<div style={{ display: 'flex', justifyContent: 'space-between' }}>
+										{
+											editClicked == true && editCommentId == a.id
+												?
+												<div className="comment-edit" onClick={editComment}>수정완료</div>
+												:
+												<div className="comment-edit" onClick={() => { setEditClicked(true); setEditCommentId(a.id); setEdittingComment(a.body) }}>수정</div>
+
+										}
+										<div style={{ cursor: 'pointer' }} onClick={() => { setRecommentClicked(!recommentClicked); setRecommentId(a.id) }}>대댓글</div>
+									</div>
+								</div>
+
+							</div>
+							{
+
+								props.recommentList.filter((e) => e.comment == a.id).map((a, i) => {
+									return (
+										<div key={i} className='recomment'>
+											<p>↳ {a.body}</p>
+											<p>작성자 : {a.user}</p>
+										</div>
+									)
+								})
+
+							}
+							{
+								recommentClicked == true && recommentId == a.id
+									?
+									<Recomment id={a.id} accessToken={props.accessToken}></Recomment>
+									: ''
+							}
+						</div>
+					)
+				})
+			}
+
+
+
+			<InputGroup className="mb-3" style={{ height: '44px', width: '100%', minWidth: '350px' }}>
+				<Form.Control
+					placeholder="댓글을 작성해주세요"
+					aria-label="Recipient's username"
+					aria-describedby="basic-addon2"
+					onClick={(e) => { e.target.value = '' }}
+					onChange={(e) => { setComment(e.target.value) }}
+				/>
+
+				<Button variant="outline-secondary" id="button-addon2" onClick={() => {
+					axios.defaults.headers.common['Authorization'] = `Bearer ${props.accessToken}`
+					axios.post('/comment/', {
+						body: comment,
+						article: props.id
+					})
+						.then((res) => {
+							if (res.status == 201) {
+								console.log(res)
+								window.location.reload()
+							}
+
+						})
+						.catch((err) => {
+							console.log(err)
+							if (err.response.status == 401) {
+								alert('로그인 후 댓글 작성이 가능합니다')
+								removeRefreshToken();
+								dispatch(deleteAuthToken());
+								window.location.reload()
+							}
+
+						})
+				}}>
+					등록
+				</Button>
+
+			</InputGroup>
+		</div>
+	)
 
 }
-function ArticleDetail(props){ //게시판 상세 페이지
-    let navigate = useNavigate();
-    let {id} = useParams();
-    let [title,setTitle] = useState('');
-    let [content,setContent] = useState('');
-    let [createAt,setCreateAt] = useState('');
-    let [boardId,setBoardId] = useState(0);
-    let [user,setUser] = useState('');
-    let [image,setImage] = useState([]);
+function ArticleDetail(props) { //게시판 상세 페이지
+	let navigate = useNavigate();
+	let { id } = useParams();
+	let [title, setTitle] = useState('');
+	let [content, setContent] = useState('');
+	let [createAt, setCreateAt] = useState('');
+	let [boardId, setBoardId] = useState(0);
+	let [user, setUser] = useState('');
+	let [image, setImage] = useState([]);
 
-    const accessToken=localStorage.getItem('accessToken');
-    useEffect(()=>{
-        axios.get(`/article/${id}`)
-        .then((res)=>{
-            if(res.status===200){
-                
-                if(res.data.images.length!=0){
-                    setImage(res.data.images)
-                }
-               
-                setBoardId(res.data.id)
-                setTitle(res.data.title);
-                setContent(res.data.content);
-                setCreateAt(formatDate(res.data.created_at));
-                setUser(res.data.user.username);
-            }
-            
-        })
-        .catch((err)=>{
-            console.log(err)
-        })
-   },[])
-   
-   function formatDate(date) {
-    var d = new Date(date),
-        month = '' + (d.getMonth() + 1),
-        day = '' + d.getDate(),
-        year = d.getFullYear();
+	const accessToken = localStorage.getItem('accessToken');
+	useEffect(() => {
+		axios.get(`/article/${id}`)
+			.then((res) => {
+				if (res.status === 200) {
 
-    if (month.length < 2) 
-        month = '0' + month;
-    if (day.length < 2) 
-        day = '0' + day;
+					if (res.data.images.length != 0) {
+						setImage(res.data.images)
+					}
 
-    return [year, month, day].join('-');
-    }
+					setBoardId(res.data.id)
+					setTitle(res.data.title);
+					setContent(res.data.content);
+					setCreateAt(formatDate(res.data.created_at));
+					setUser(res.data.user.username);
+				}
 
-    function deleteArticle(){
-        axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`
-        axios.delete(`/article/${id}`)
-        .then((res)=>{
-            if(res.status==204){
-                alert('삭제되었습니다.')
-                navigate('/board/list')
-            }
-            
-        })
-        .catch((err)=>{
-            if(err.response.status==403){
-                alert('본인의 게시물만 삭제 할 수 있습니다!')
-            }
-            else if(err.response.status==401){
-                alert('권한이 없습니다!')
-                window.location.reload();
-            }
-        })
-    }
+			})
+			.catch((err) => {
+				console.log(err)
+			})
+	}, [])
 
-    function checkAuth(){
-        if(accessToken!=null){
-            navigate(`/edit/${id}`)
-        }else{
-            alert('로그인 후 수정 할 수 있습니다')
-        }
-    }
+	function formatDate(date) {
+		var d = new Date(date),
+			month = '' + (d.getMonth() + 1),
+			day = '' + d.getDate(),
+			year = d.getFullYear();
 
-    
-    return(
-        <>
-        
-        <div className='detail-top'>
-            <div className='detail-header'>
-                <div style={{fontWeight:'bold',fontSize:'larger'}}>{title}</div>
-                <div className="header-detail">
-                    <div>작성일자 : {createAt}</div>
-                    <div>{user}</div>
-                </div>
-            </div>
-            <hr></hr>
-            <div className='button-container'>
-                <Button variant="light" onClick={checkAuth}>수정</Button>
-                <Button variant="danger" onClick={deleteArticle}>삭제</Button>
-            </div>
-            <div>
-                {
-                    image.length!=0
-                    ?
-                    <div className='img-container'>
-                        {
-                           
-                            image.map(function(a,i){
-                                return(
-                                    <img key={i} src={a.image}></img>
-                                )
-                            })
-                        }
-                        
-                    </div>
-                    :
-                    null
-                }
-                <div style={{minHeight:"150px",fontSize:'large'}}>{content}</div>
-            </div>
-            <hr></hr>
-            <div style={{display:'flex', justifyContent:'center'}}>
-                <Button variant="light" onClick={()=>{navigate('/board/list'); props.setCategory(0)}}>목록</Button>
-            </div>
-            <Comment id={id} accessToken={accessToken}></Comment>
-            
-        </div>
-        </>
-    )
+		if (month.length < 2)
+			month = '0' + month;
+		if (day.length < 2)
+			day = '0' + day;
+
+		return [year, month, day].join('-');
+	}
+
+	function deleteArticle() {
+		axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`
+		axios.delete(`/article/${id}`)
+			.then((res) => {
+				if (res.status == 204) {
+					alert('삭제되었습니다.')
+					navigate('/board/list')
+				}
+
+			})
+			.catch((err) => {
+				if (err.response.status == 403) {
+					alert('본인의 게시물만 삭제 할 수 있습니다!')
+				}
+				else if (err.response.status == 401) {
+					alert('권한이 없습니다!')
+					window.location.reload();
+				}
+			})
+	}
+
+	function checkAuth() {
+		if (accessToken != null) {
+			navigate(`/edit/${id}`)
+		} else {
+			alert('로그인 후 수정 할 수 있습니다')
+		}
+	}
+
+
+	return (
+		<>
+
+			<div className='detail-top'>
+				<div className='detail-header'>
+					<div style={{ fontWeight: 'bold', fontSize: 'larger' }}>{title}</div>
+					<div className="header-detail">
+						<div>작성일자 : {createAt}</div>
+						<div>{user}</div>
+					</div>
+				</div>
+				<hr></hr>
+				<div className='button-container'>
+					<Button variant="light" onClick={checkAuth}>수정</Button>
+					<Button variant="danger" onClick={deleteArticle}>삭제</Button>
+				</div>
+				<div>
+					{
+						image.length != 0
+							?
+							<div className='img-container'>
+								{
+
+									image.map(function (a, i) {
+										return (
+											<img key={i} src={a.image}></img>
+										)
+									})
+								}
+
+							</div>
+							:
+							null
+					}
+					<div style={{ minHeight: "150px", fontSize: 'large' }}>{content}</div>
+				</div>
+				<hr></hr>
+				<div style={{ display: 'flex', justifyContent: 'center' }}>
+					<Button variant="light" onClick={() => { navigate('/board/list'); props.setCategory(0) }}>목록</Button>
+				</div>
+				<Comment id={id} accessToken={accessToken} recommentList={props.recommentList}></Comment>
+
+			</div>
+		</>
+	)
 }
 
 export default ArticleDetail
