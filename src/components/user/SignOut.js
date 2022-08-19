4343922faf2adb8adee0c4633f7fd590dd25aed3
mincheolsong <thdalscjf05@naver.com>
2022-08-19 13:59:30 +0900

    Update UserId localStorage

--- src/components/user/SignOut.js
+++ src/components/user/SignOut.js
@@ -1,29 +1,18 @@
-import { useNavigate } from 'react-router';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import {Button} from 'react-bootstrap'
 import { getRefreshToken,removeRefreshToken } from '../../cookie/Cookie';
 import { deleteAuthToken } from '../../store';
 
 function SignOut(){
-    let navigate = useNavigate();
+
     const dispatch = useDispatch();
     
     function logout(){
         removeRefreshToken();
         dispatch(deleteAuthToken());
-        //localStorage.removeItem('userId')
+        localStorage.removeItem('UserID')
         window.location.reload()
         alert('로그아웃 되었습니다');
-        
-        // const response = await logoutUser(refreshToken);
-        
-        // if(response.status){
-        //     dispatch(deleteToken());
-        //     removeCookieToken();
-        //     return navigate('/signup');
-        // }else{
-        //     alert('로그아웃 실패ㅜ.ㅜ');
-        // }
     }
    
 
